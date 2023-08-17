359e172ab6f2891a8cefecd2c45c9c1fd774d02f
Shoyur <100440995+Shoyur@users.noreply.github.com>
2023-08-17 02:01:37 -0400

    ...

--- 19_date_time.php
+++ 19_date_time.php
@@ -1,11 +1,12 @@
 
 <?php
 
-
+date_default_timezone_set('US/Eastern');
 $maintenant = time();
 echo $maintenant . '</br>'; // integer, nombres de secondes depuis jan 1970
 echo $maintenant + 5*24*60*60 . '</br>'; // dans 5 jours
 echo date('d/m/Y H:m:s') . '</br>';
+echo date_default_timezone_get() . '</br>';
 
 
 // changer ou intteroger le timezone
